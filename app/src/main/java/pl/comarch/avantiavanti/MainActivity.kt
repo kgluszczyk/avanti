681adb2a308bffb822b94b89bf17ff59baf2da13
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2022-06-13 15:53:14 +0200

    Activity Lifecycle test

--- app/src/main/java/pl/comarch/avantiavanti/MainActivity.kt
+++ app/src/main/java/pl/comarch/avantiavanti/MainActivity.kt
@@ -2,6 +2,7 @@ package pl.comarch.avantiavanti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -14,6 +15,32 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             Toast.makeText(this, "Zagłosowano", Toast.LENGTH_LONG).show()
         }
+        Log.i("Lifecycle", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "onDestroy")
     }
 }
 
