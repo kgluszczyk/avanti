7c6497ce7abc297c6ce00f3f005f68b527df41b9
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2022-06-13 16:49:10 +0200

    Implict intent

--- app/src/main/java/pl/comarch/avantiavanti/SummaryActivity.kt
+++ app/src/main/java/pl/comarch/avantiavanti/SummaryActivity.kt
@@ -1,5 +1,6 @@
 package pl.comarch.avantiavanti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -8,6 +9,18 @@ class SummaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
-        findViewById<TextView>(R.id.totalScore).text = intent.getIntExtra("TotalScore", -1).toString()
+        with(findViewById<TextView>(R.id.totalScore)){
+            text = intent.getIntExtra("TotalScore", -1).toString()
+            setOnLongClickListener {
+                Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, text)
+                    type = "text/plain"
+                }.also {
+                    startActivity(it)
+                }
+                true
+            }
+        }
     }
 }
