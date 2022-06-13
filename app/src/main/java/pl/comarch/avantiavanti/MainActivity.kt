7ea1f3c69d43be4e1a6fc1d07d7124190d8c4c8a
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2022-06-13 16:15:44 +0200

    Save state

--- app/src/main/java/pl/comarch/avantiavanti/MainActivity.kt
+++ app/src/main/java/pl/comarch/avantiavanti/MainActivity.kt
@@ -5,17 +5,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var totalScore: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        totalScore = savedInstanceState?.getInt("TotalScore") ?: totalScore
         val button = findViewById<Button>(R.id.action)
         button.setOnClickListener {
+            totalScore = findViewById<EditText>(R.id.scoreTeamLeft).text.toString().toInt() +
+                    findViewById<EditText>(R.id.scoreTeamRight).text.toString().toInt()
             Toast.makeText(this, "Zagłosowano", Toast.LENGTH_LONG).show()
         }
         Log.i("Lifecycle", "onCreate")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("TotalScore", totalScore)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
@@ -25,6 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        Toast.makeText(this, "TotalScore: $totalScore", Toast.LENGTH_LONG).show()
         Log.i("Lifecycle", "onResume")
     }
 
