464236979d9464846ba403dddc4aed5aa5d18397
Krzysztof Głuszczyk <krzysztof.gluszczyk@gmail.com>
2022-06-14 11:09:31 +0200

    RecyclerView - views

---
+++ app/src/main/java/pl/comarch/avantiavanti/MatchesActivity.kt
@@ -0,0 +1,11 @@
+package pl.comarch.avantiavanti
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MatchesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matches)
+    }
+}
